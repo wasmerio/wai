4a2a900c56c0ddbb34e6bb1777733f8aa6a9e0e4
Felix Schütt <felix@wasmer.io>
2022-09-15 18:12:10 +0200

    Publish some crates on crates.io (figure out dep order)

--- publish.sh
+++ publish.sh
@@ -1,3 +1,12 @@
-cargo publish --manifest-path="$(pwd)/crates/parser/Cargo.toml"
-cargo publish --manifest-path="$(pwd)/crates/gen-core/Cargo.toml"
-cargo publish --manifest-path="$(pwd)/crates/wit-component/Cargo.toml"
+# cargo publish --manifest-path="$(pwd)/crates/parser/Cargo.toml"
+# cargo publish --manifest-path="$(pwd)/crates/gen-core/Cargo.toml"
+# cargo publish --manifest-path="$(pwd)/crates/wit-component/Cargo.toml"
+# cargo publish --allow-dirty --manifest-path="$(pwd)/crates/gen-rust/Cargo.toml"
+
+# cargo publish --manifest-path="$(pwd)/crates/test-helpers/Cargo.toml"#
+cargo publish --allow-dirty --manifest-path="$(pwd)/crates/gen-rust-wasm/Cargo.toml"
+# cargo publish --allow-dirty --manifest-path="$(pwd)/crates/rust-wasm-impl/Cargo.toml"
+# cargo publish --allow-dirty --manifest-path="$(pwd)/crates/rust-wasm/Cargo.toml"
+# cargo publish --allow-dirty --manifest-path="$(pwd)/crates/gen-rust-wasm/Cargo.toml"
+# cargo publish --allow-dirty --manifest-path="$(pwd)/crates/rust-wasm-impl/Cargo.toml"
+
